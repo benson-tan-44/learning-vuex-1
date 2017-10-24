1bf53e68a3d6f6c68b1bb19944c36f611e0c7848
bensontan44 <benson.tan.44@gmail.com>
2017-10-23 20:51:12 -0400

    Genre mutations and actions are correctly working with the select v-model now

--- src/store/modules/base.js
+++ src/store/modules/base.js
@@ -2,7 +2,9 @@ import Vue from 'vue';
 
 const state = {
   apiKey: '2519bad98acfb01294fc145600974941',
-  result:[]
+  result:[],
+  genres:[],
+  userGenre:null
 
 
 }
@@ -11,6 +13,14 @@ const getters = {
 
   result: state => {
     return state.result;
+  },
+
+  genres: state => {
+    return state.genres;
+  },
+
+  userGenre: state => {
+    return state.userGenre;
   }
 
 }
@@ -19,18 +29,30 @@ const mutations = {
 
 setSource(state, result) {
   state.result = result;
-}
+},
 
+setGenre(state, genres) {
+  state.genres = genres;
+},
+
+setUserGenre(state, selectedGenre ) {
+  state.userGenre = selectedGenre;
+}
 
 }
 
 const actions = {
-  initAPI: ({commit}) => {
+  initGenres: ({commit}) => {
     Vue.http.get('https://api.themoviedb.org/3/genre/movie/list?api_key=' + state.apiKey ).then((response) => {
-    commit('setSource',  response.body );
+    commit('setGenre',  response.body.genres );
   })
 
+  },
+
+  changeGenres: ({commit}, selectedGenre) => {
+    commit('setUserGenre', selectedGenre);
   }
+
 
 
 }
