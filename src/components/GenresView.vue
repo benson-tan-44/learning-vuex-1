aa9fc1426f22623a6992bb0c713acd1e449f62c8
bensontan44 <benson.tan.44@gmail.com>
2017-10-31 19:38:09 -0400

    Set up genres component and adding bubbles after

---
+++ src/components/GenresView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-info">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          <strong>{{genre.name}}</strong>
+        </h3>
+      </div>
+      <!--
+      <div class="panel-body">
+        {{item.desc}}
+      </div>
+    -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['genre']
+}
+
+</script>
+
+<style scoped>
+.panel{
+  height:200px;
+}
+</style>
